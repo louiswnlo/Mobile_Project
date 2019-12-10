aeb466783e882d853756dff9db42681cbd08b9bc
louiswnlo <louiswn.lo@tinklabs.com>
2019-12-10 21:41:58 +0800

    compress jpg image

--- app/src/main/java/cs/hku/hk/mobileproject/ShopList.java
+++ app/src/main/java/cs/hku/hk/mobileproject/ShopList.java
@@ -2,24 +2,29 @@ package cs.hku.hk.mobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ShopList extends AppCompatActivity {
 
     ListView mListView;
 
-    // Test Data
+    // Test Data --> Replace with Database data
     int[] images = {R.drawable.shop_img_1,
                     R.drawable.shop_img_2,
                     R.drawable.shop_img_3,
                     R.drawable.shop_img_4,
                     R.drawable.shop_img_5,
                     R.drawable.shop_img_6 };
+
+    String[] shopnames = {"Foot Spa Jordan", "Levo Spa", "The Foot Room",
+                          "Beauty Parade", "Float on Hong Kong", "Charming Beuty & Slimming"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,10 +60,12 @@ public class ShopList extends AppCompatActivity {
             View mView = getLayoutInflater().inflate(R.layout.layout_shop_list, null);
 
             ImageView mImageView = (ImageView) mView.findViewById(R.id.bg_img);
+            TextView shopnameTextView = (TextView) mView.findViewById(R.id.shopname_txt);
 
             mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-
             mImageView.setImageResource(images[i]);
+
+            shopnameTextView.setText(shopnames[i]);
 
             return mView;
         }
